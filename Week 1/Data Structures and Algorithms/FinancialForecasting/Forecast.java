88706489c82a296a7d2182ae1934197ddcec9bb1
LakshmiPathi G <lakshmipathig007@gmail.com>
2025-07-20 23:06:01 +0530

    programs

--- Week 1/Data Structures and Algorithms/FinancialForecasting/Forecast.java
+++
@@ -1,8 +0,0 @@
-public class Forecast {
-    public static double futureValue(double presentValue, double rate, int years) {
-        if (years == 0) {
-            return presentValue;
-        }
-        return futureValue(presentValue * (1 + rate), rate, years - 1);
-    }
-}
